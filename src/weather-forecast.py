17a41c615279edd78727218a2f4dab37c9f3f424
dbd8 <daniel-1989@gmx.de>
2020-08-20 01:17:54 +0200

    feat: added rain_in_next_timewindow function

--- src/weather-forecast.py
+++ src/weather-forecast.py
@@ -1,4 +1,22 @@
 import requests
+import datetime
+
+
+def rain_in_next_timewindow(weather_data, time_window) -> bool:
+    current_time = datetime.datetime.now()
+
+    for hours in range(len(weather_data["hourly"])):
+        if (datetime.datetime.fromtimestamp(
+                weather_data["hourly"][hours]["dt"]) - current_time).total_seconds() <= time_window * 3600:
+            if weather_data["hourly"][hours]["weather"][0]["main"] == "Rain":
+                print(datetime.datetime.fromtimestamp(weather_data["hourly"][hours]["dt"]))
+                print(weather_data["hourly"][hours]["weather"][0]["description"])
+                return True
+
+    return False
+
+
+time_window = 12
 
 lat = "49.2627"
 lon = "9.2995"
@@ -13,3 +31,13 @@ r = requests.get(open_weather_map_url)
 weather_data = r.json()
 
 print(weather_data)
+
+# for hours in range(len(weather_data["hourly"])):
+#     if weather_data["hourly"][hours]["weather"][0]["main"] == "Rain":
+#         print(weather_data["hourly"][hours]["dt"])
+#         readable = datetime.datetime.fromtimestamp(weather_data["hourly"][hours]["dt"])
+#         print(readable)
+#         print(weather_data["hourly"][hours]["weather"][0]["main"])
+#         print(weather_data["hourly"][hours]["weather"][0]["description"])
+
+print(rain_in_next_timewindow(weather_data, time_window))
